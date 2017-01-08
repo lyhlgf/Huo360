027e660d9fe79c80356b37dfed3956dd90a37425
Yuehan Liu <lyhlgf93@gmail.com>
2017-01-08 19:55:48 +0800

    Initial Commit

---
+++ Huo360Tests/Huo360Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Huo360Tests.swift
+//  Huo360Tests
+//
+//  Created by Cloud on 17/1/8.
+//  Copyright © 2017年 Cloud. All rights reserved.
+//
+
+import XCTest
+@testable import Huo360
+
+class Huo360Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
